3702cb3fb62ba0540116c1b9bb934ce14b2d1e68
shixh <shixh@si-tech.com.cn>
2023-04-21 17:31:09 +0800

    提交

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" autocomplete="on" label-position="left">
-
+    <el-form
+      ref="loginForm"
+      :model="loginForm"
+      :rules="loginRules"
+      class="login-form"
+      autocomplete="on"
+      label-position="left"
+    >
       <div class="title-container">
         <h3 class="title">Login Form</h3>
       </div>
@@ -21,7 +27,12 @@
         />
       </el-form-item>
 
-      <el-tooltip v-model="capsTooltip" content="Caps lock is On" placement="right" manual>
+      <el-tooltip
+        v-model="capsTooltip"
+        content="Caps lock is On"
+        placement="right"
+        manual
+      >
         <el-form-item prop="password">
           <span class="svg-container">
             <svg-icon icon-class="password" />
@@ -40,36 +51,32 @@
             @keyup.enter.native="handleLogin"
           />
           <span class="show-pwd" @click="showPwd">
-            <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+            <svg-icon
+              :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"
+            />
           </span>
         </el-form-item>
       </el-tooltip>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+      <el-button
+        :loading="loading"
+        type="primary"
+        style="width: 100%; margin-bottom: 30px"
+        @click.native.prevent="handleLogin"
+        >Login</el-button
+      >
 
-      <div style="position:relative">
+      <div style="position: relative">
         <div class="tips">
           <span>Username : admin</span>
-          <span>Password : any</span>
+          <span>Password : 111111</span>
         </div>
         <div class="tips">
-          <span style="margin-right:18px;">Username : editor</span>
-          <span>Password : any</span>
+          <span style="margin-right: 18px">Username : editor</span>
+          <span>Password : 111111</span>
         </div>
-
-        <el-button class="thirdparty-button" type="primary" @click="showDialog=true">
-          Or connect with
-        </el-button>
       </div>
     </el-form>
-
-    <el-dialog title="Or connect with" :visible.sync="showDialog">
-      Can not be simulated on local, so please combine you own business simulation! ! !
-      <br>
-      <br>
-      <br>
-      <social-sign />
-    </el-dialog>
   </div>
 </template>
 
@@ -114,7 +121,7 @@ export default {
   },
   watch: {
     $route: {
-      handler: function(route) {
+      handler: function (route) {
         const query = route.query
         if (query) {
           this.redirect = query.redirect
@@ -155,6 +162,11 @@ export default {
     handleLogin() {
       this.$refs.loginForm.validate(valid => {
         if (valid) {
+          if (this.loginForm.password !== '111111') {
+            this.loading = false
+            this.$message.error('密码错误,请重新输入');
+            return false
+          }
           this.loading = true
           this.$store.dispatch('user/login', this.loginForm)
             .then(() => {
@@ -204,8 +216,8 @@ export default {
 /* 修复input 背景不协调 和光标变色 */
 /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
-$bg:#283443;
-$light_gray:#fff;
+$bg: #283443;
+$light_gray: #fff;
 $cursor: #fff;
 
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
@@ -248,9 +260,9 @@ $cursor: #fff;
 </style>
 
 <style lang="scss" scoped>
-$bg:#2d3a4b;
-$dark_gray:#889aa4;
-$light_gray:#eee;
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
 
 .login-container {
   min-height: 100%;
---
+++ src/views/video/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <Video
+    v-show="true"
+    :videoSrc="videoSrc"
+    :videoPoster="videoPoster"
+    :width="1200"
+    :height="500"
+    :autoplay="true"
+    :controls="true"
+    :loop="false"
+    :muted="false"
+    preload="auto"
+    :showPlay="true"
+    :playWidth="96"
+    zoom="contain"
+  />
+</template>
+
+<script>
+
+import Video from './video'
+export default {
+  components: {
+    Video
+  },
+  data() {
+    return {
+      // 视频链接支持https或require本地地址
+      videoSrc: require('@/assets/video/画江湖mv.mp4'),
+      // 视频封面支持https或require本地地址
+      videoPoster: require('@/assets/video/fengmian.jpg')
+    }
+  }
+
+
+}
+</script>
+
+<style>
+</style>
